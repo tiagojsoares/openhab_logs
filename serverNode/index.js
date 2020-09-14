e6416868c020b355c67637761d687e6037800125
Tiago Soares <tiago.soares@ontrollerbms.com.br>
2020-09-14 08:35:59 -0300

    api node

--- serverNode/index.js
+++ serverNode/index.js
@@ -2,7 +2,7 @@ console.log('Rodando');
 const express = require('express');
 const app = express();
 const http = require("http");
-const port = process.env.Port || 3000;
+const port = process.env.Port || 80;
 const server = http.createServer(app);
 const rest = require('./rest');
 
